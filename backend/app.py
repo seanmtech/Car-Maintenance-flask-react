4dc2c75896011f04cba3c274c80906037742bc52
seanmtech <70730917+seanmtech@users.noreply.github.com>
2023-10-18 23:47:39 -0500

    add post route, change get route

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 import os 
 import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,26 +11,47 @@ db_password = os.environ.get('DB_PASSWORD')
 
 app = Flask(__name__)
 
-# TODO: configure connection string
-    # get formatting from https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/config/ 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{db_password}@localhost/flaskCarDB'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+marsh = Marshmallow(app)
 
 class CarsTable(db.Model):
     carID = db.Column(db.Integer, primary_key=True)
     carModel = db.Column(db.String(100))
+    mileage = db.Column(db.Integer)
     dateMilesEntered = db.Column(db.DateTime, default = datetime.datetime.now)
 
-    def __init__(self, title, body):
-        self.title = title
-        self.body = body
+    def __init__(self, carModel, mileage):
+        self.carModel = carModel
+        self.mileage = mileage
 
-@app.route('/', methods = ['GET'])
-def get_articles():
-    return jsonify({"Hello": "World"})
+class CarsSchema(marsh.Schema):
+    class Meta:
+        fields = ('carID', 'carModel', 'dateMilesEntered', 'mileage')
+
+car_schema = CarsSchema()
+carQuerySet_Schema = CarsSchema(many=True)
+
+@app.route('/get', methods = ['GET'])
+def get_cars():
+    allCarsInDB = CarsTable.query.all()
+    results = carQuerySet_Schema.dump(allCarsInDB)
+    return jsonify(results)
+
+
+@app.route('/add', methods = ['POST'])
+def add_car():
+    carModel = request.json['carModel']
+    mileage = request.json['mileage']
+
+    carListForDB = CarsTable(carModel, mileage)
+    db.session.add(carListForDB)
+    db.session.commit()
+    return car_schema.jsonify(carListForDB)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
